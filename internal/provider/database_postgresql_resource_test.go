5afc46882da548be1661c9c209c2503465229172
Paul Kerspe <paul.kerspe@utimaco.com>
2026-05-02 17:26:56 +0200

    fix test values

--- internal/provider/database_postgresql_resource_test.go
+++ internal/provider/database_postgresql_resource_test.go
@@ -35,8 +35,8 @@ resource "databasus_database_postgresql" "test" {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("databasus_database_postgresql.test", "name", "test-postgres-db"),
 					resource.TestCheckResourceAttr("databasus_database_postgresql.test", "database", "test_db"),
-					resource.TestCheckResourceAttr("databasus_database_postgresql.test", "host", "db"),
-					resource.TestCheckResourceAttr("databasus_database_postgresql.test", "port", "5432"),
+					resource.TestCheckResourceAttr("databasus_database_postgresql.test", "host", "localhost"),
+					resource.TestCheckResourceAttr("databasus_database_postgresql.test", "port", "8432"),
 					resource.TestCheckResourceAttr("databasus_database_postgresql.test", "is_https", "false"),
 
 					resource.TestCheckResourceAttrSet("databasus_database_postgresql.test", "username"),
